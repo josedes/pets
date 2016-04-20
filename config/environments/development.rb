f4271510f9d0e82f0c7ab41eb89ac7ad43e01647
josedes <josedes@gmail.com>
2016-04-20 10:57:19 -0430

    se arreglo un problema en development.rb, se agrego index.html y acercade.html

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,13 +49,16 @@ Rails.application.configure do
   config.assets.debug = true
 
   config.action_mailer.smtp_settings = { 
-     :address => "smtp.gmail.com", 
-     :port => 587, 
-     :domain =>  ganajo.com  
-     :user_name => josedes, 
-     :password => gaby10786452,
-     authentication: "plain",
-     enable_starttls_auto: true  
+ 
  } 
  
+   config.action_mailer.smtp_settings = { 
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: ENV["DOMAIN_NAME"],
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
+  }
 end
